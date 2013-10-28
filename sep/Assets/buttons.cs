bab648b2b4f91e6a839ea715a9b8d808a5a9d0d6
Johannes Martens <martens.johannes@gmail.com>
2013-10-28 10:04:06 +0100

    added buttons allowing spawning of towers

---
+++ sep/Assets/buttons.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class buttons : MonoBehaviour {
+
+
+    Texture2D boxIcon;
+    Texture2D sphereIcon;
+    Texture2D cylinderIcon;
+	GameObject building;
+	bool isHoverGUI = false; 
+	Rect rect =new Rect (10,10, 110, 50);
+
+ 
+ 
+ 
+ 
+    void OnGUI () 
+    {
+        if (GUI.Button (new Rect (10,10, 50, 50),"1" )) 
+        { building = GameObject.Find("TowerBullet");
+		}
+		
+		if (GUI.Button (new Rect (60,10, 50, 50),"2" )) 
+        { building = GameObject.Find("TowerLaser");
+		}
+ 
+     isHoverGUI = rect.Contains( Event.current.mousePosition );
+    }
+ 
+    void Update () {
+ 
+ 
+    if(Input.GetMouseButtonDown(0)&& !isHoverGUI)
+    {
+ 
+ //   var hit = RaycastHit;
+    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+	RaycastHit hit;
+ 
+    if(Physics.Raycast(ray, out hit) && hit.collider.tag == "spawnarea")
+    {
+ 	Instantiate(building,hit.point,Quaternion.identity);
+ 
+ 
+    
+           }
+        }
+}}
