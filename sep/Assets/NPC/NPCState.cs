5efdbb565455988476689c4c53e595e337824640
David Asmuth <piranha771@gmail.com>
2013-11-08 10:50:18 +0100

    Added EvilBitModel; Added BitStates; AddedBitStateController; Added Materials for all BitStates;

---
+++ sep/Assets/NPC/NPCState.cs
@@ -0,0 +1,12 @@
+﻿
+/// <summary>
+/// Defines the different states that a NPC can be in
+/// </summary>
+public enum NPCState {
+    Unknown,
+    Good,
+    Evil,
+    SuperGood,
+    SuperEvil
+}
+
