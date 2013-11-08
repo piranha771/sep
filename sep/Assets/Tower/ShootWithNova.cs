f8305e5001be281f2a1d7e43e9999a25179607c9
Vladimir Malikov <v.malikov.hamburg@gmail.com>
2013-11-08 12:27:56 +0100

    Tower nova work in alpha status.

---
+++ sep/Assets/Tower/ShootWithNova.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShootWithNova : MonoBehaviour, IShootWith {
+    private GameObject npc;
+    private int weaponDamage = 15;
+    private bool npcState = false;
+    private float radiusDelay = 1f;
+    private float delay = 1f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+      
+	
+	}
+
+    public void Shoot(GameObject npcEnemy, GameObject towerWeapon) {
+
+        makeRadius();
+        
+    }
+
+    public void StopShooting() {
+        npcState = false;
+        Vector3 oldScale = new Vector3(0.1f,0.1f,0.1f);
+        transform.localScale = oldScale; 
+    }
+
+    public int WeaponDamage {
+        get {
+            return weaponDamage;
+        }
+        set {
+            weaponDamage = value;
+        }
+    }
+
+    private void makeRadius() {
+
+        while (radiusDelay > 0) {
+            radiusDelay -= Time.deltaTime;
+            Vector3 newScale = new Vector3(0.0025f, 0.0025f, 0.0025f);
+
+            transform.localScale = transform.localScale + newScale;
+        }
+        radiusDelay = delay;
+
+    }
+}
