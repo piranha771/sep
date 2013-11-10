d8585026a4f6321373ee1443294c22e2ecc92ca7
SWindrath <steffen.windrath@haw-hamburg.de>
2013-11-10 21:20:12 +0100

    implemented rudimentary GUI. Outsourced Gui functionality for CPU Heat and Income to their own scripts.

---
+++ sep/Assets/GUI/GuiCPUHeat.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GuiCPUHeat : MonoBehaviour {
+	
+	void OnGUI (){
+		CPUHeatController controller = GameObject.Find("CPU").GetComponent<CPUHeatController>();
+		string heat = controller.getCurrentTemp().ToString("0.0");
+		string displayText = "CPU Heat\r\n" + heat + " C°";
+		var centeredStyle = GUI.skin.GetStyle("Label");
+		centeredStyle.alignment = TextAnchor.UpperCenter;
+		GUI.Label (new Rect (Screen.width-100, 10, 100, 50), displayText, centeredStyle);
+    }
+	
+	void Start () {}
+ 
+    void Update () {}
+}
---
+++ sep/Assets/GUI/GuiCPUIncome.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GuiCPUIncome : MonoBehaviour {
+
+	void OnGUI (){
+		CPUIncomeController controller = GameObject.Find("CPU").GetComponent<CPUIncomeController>();
+		string displayText = "Computing time\r\n" + controller.getComputingTime() + " ms";
+		var centeredStyle = GUI.skin.GetStyle("Label");
+		centeredStyle.alignment = TextAnchor.UpperCenter;
+		GUI.Label (new Rect (Screen.width-100, 40, 100, 50), displayText, centeredStyle);
+    }
+	
+	void Start () {}
+ 
+    void Update () {}
+}
