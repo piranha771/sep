27ba4c8656e421af0bd7c996d8305cbdfa837846
Steffen Windrath <rankin@web.de>
2013-11-21 11:31:00 +0100

    some GUI Refactorisation: Placement relative to Resolution & scalable, Buttons only enabled if enough money for action

--- sep/Assets/GUI/GuiCPUHeat.cs
+++ sep/Assets/GUI/GuiCPUHeat.cs
@@ -3,16 +3,52 @@ using System.Collections;
 
 public class GuiCPUHeat : MonoBehaviour {
 	
+	public float Scale;
+	public float xPositionInPercent;
+	public float yPositionInPercent;
+	
+	private int minBars = 10; //This Variable will set the number of Bars that are representing the minimun heat of the CPU.
+							  //These will always be visible, even if the current heat is lower than the minimum.
+	private float xPositionModifier;
+	private float yPositionModifier;
+	
+	public Texture TempDisplayContainer;
+	public Texture[] bars;
+	
 	void OnGUI (){
+		int currentHeight = Screen.height;
+		int currentWidth = Screen.width;
+		int xPosition = (int)(currentWidth*xPositionModifier);
+		int yPosition = (int)(currentHeight*yPositionModifier);
+		
 		CPUHeatController controller = GameObject.Find("CPU").GetComponent<CPUHeatController>();
-		string heat = controller.CurrentTemp.ToString("0.0");
+		string heat = controller.CurrentTemp.ToString("0.0");//Current Heat with one fractional digit.
+		float normalHeat = controller.NormalHeat;
+		float maxHeat = controller.MeltdownTemp;
+		
+		float heatPerBar = (maxHeat-normalHeat)/(float)(bars.Length-minBars);
+		
 		string displayText = "CPU Heat\r\n" + heat + " C°";
 		var centeredStyle = GUI.skin.GetStyle("Label");
 		centeredStyle.alignment = TextAnchor.UpperCenter;
+		
 		GUI.Label (new Rect (Screen.width-100, 10, 100, 50), displayText, centeredStyle);
+		GUI.DrawTexture(new Rect(xPosition,yPosition,60*Scale,60*Scale),TempDisplayContainer, ScaleMode.StretchToFill);
+		for (int i = 0; i<minBars;i++){
+			GUI.DrawTexture(new Rect(xPosition,yPosition,60*Scale,60*Scale),bars[i], ScaleMode.StretchToFill);
+		}
+		
+		for (int i = minBars; i<bars.Length;i++){
+			if(controller.CurrentTemp-normalHeat>=heatPerBar*(i-minBars+1)){
+				GUI.DrawTexture(new Rect(xPosition,yPosition,60*Scale,60*Scale),bars[i], ScaleMode.StretchToFill);
+			}
+		}
     }
 	
-	void Start () {}
+	void Start () {
+		yPositionModifier = yPositionInPercent/100.0f;
+		xPositionModifier = xPositionInPercent/100.0f;
+	}
  
     void Update () {}
 }
--- sep/Assets/GUI/GuiTowerSpawnMenu.cs
+++ sep/Assets/GUI/GuiTowerSpawnMenu.cs
@@ -22,6 +22,14 @@ public class GuiTowerSpawnMenu : MonoBehaviour {
 	
 	public bool buyMode = true;
 	public bool upMode = false;
+	
+	public float xPositionInPercent;
+	public float yPositionInPercent;
+	
+	private float xPositionModifier;
+	private float yPositionModifier;
+	
+	public float scale;
 
 
     public GameObject TowerBullet { get { return towerBullet; } set { towerBullet = value; } }
@@ -36,60 +44,89 @@ public class GuiTowerSpawnMenu : MonoBehaviour {
 		
 		if (buyMode) {
             CPUComputeTimeController controller = GetComponent<CPUComputeTimeController>();
-		    TowerCost costTable = GameObject.Find("CPU").GetComponent<TowerCost>();		
-		    int time = controller.CPUTime;
-
-            if (GUI.Button(new Rect(10, 10, 50, 50), "TB") && time >= towerBullet.GetComponent<TowerCost>().CPUTimeCost) {
-                controller.CPUTime -= towerBullet.GetComponent<TowerCost>().CPUTimeCost;
-                selectedTower = towerBullet;
-            }
-
-            if (GUI.Button(new Rect(60, 10, 50, 50), "TL") && time >= towerLaser.GetComponent<TowerCost>().CPUTimeCost) {
-                controller.CPUTime -= towerLaser.GetComponent<TowerCost>().CPUTimeCost;
-                selectedTower = towerLaser;
-            }
-
-            if (GUI.Button(new Rect(110, 10, 50, 50), "TG") && time >= towerGatling.GetComponent<TowerCost>().CPUTimeCost) {
-                controller.CPUTime -= towerGatling.GetComponent<TowerCost>().CPUTimeCost;
-                selectedTower = towerGatling;
-            }
-
-            if (GUI.Button(new Rect(160, 10, 50, 50), "T4B") && time >= towerFourBurrelGun.GetComponent<TowerCost>().CPUTimeCost) {
-                controller.CPUTime -= towerFourBurrelGun.GetComponent<TowerCost>().CPUTimeCost;
-                selectedTower = towerFourBurrelGun;
-            }
-
-            if (GUI.Button(new Rect(210, 10, 50, 50), "TN") && time >= towerNova.GetComponent<TowerCost>().CPUTimeCost) {
-                controller.CPUTime -= towerNova.GetComponent<TowerCost>().CPUTimeCost;
-                selectedTower = towerNova;
-            }
-
-            if (GUI.Button(new Rect(260, 10, 50, 50), "TD") && time >= towerDetector.GetComponent<TowerCost>().CPUTimeCost) {
-                controller.CPUTime -= towerDetector.GetComponent<TowerCost>().CPUTimeCost;
-                selectedTower = towerDetector;
-            }
+			
+			int currentHeight = Screen.height;
+			int currentWidth = Screen.width;
+			int xPosition = (int)(currentWidth*xPositionModifier);
+			int yPosition = (int)(currentHeight*yPositionModifier);
+			
+			
+			//TowerBullet
+			TowerButtonCreate(towerBullet,"TB",xPosition,yPosition,controller);			
+			
+			//TowerLaser
+			TowerButtonCreate(towerLaser,"TL",50+xPosition,yPosition,controller);
+			
+			//TowerGatling
+			TowerButtonCreate(towerGatling,"TG",100+xPosition,yPosition,controller);			
+			
+			//TowerFourBarrel
+			TowerButtonCreate(towerFourBurrelGun,"T4",150+xPosition,yPosition,controller);		
+			
+			//TowerNova
+			TowerButtonCreate(towerNova,"TN",200+xPosition,yPosition,controller);		
+			
+			//TowerDetector
+			TowerButtonCreate(towerDetector,"TD",250+xPosition,yPosition,controller);
+		
  
             isHoverGUI = rect.Contains( Event.current.mousePosition );
         }
 		
 		if (upMode) {
+			
+			//DMG
+			if(false) {GUI.enabled = false;}
+			
 			 if (GUI.Button(new Rect(10, 60, 50, 50), "DMG")) {
 				//TODO
-		}
+			}
+			GUI.enabled = true;
+			
+			//RNG
+			if(false) {GUI.enabled = false;}
+			
 			 if (GUI.Button(new Rect(60, 60, 50, 50), "RNG")) {
 				//TODO
-		}
+			}
+			GUI.enabled = true;
+			
+			//SPD
+			if(false) {GUI.enabled = false;}
+			
 			 if (GUI.Button(new Rect(110, 60, 50, 50), "SPD")) {
 				//TODO
-		}
+			}
+			GUI.enabled = true;
+			
+			//SLL
+			if(false) {GUI.enabled = false;}
+			
 			 if (GUI.Button(new Rect(160, 60, 50, 50), "SLL")) {
 				//TODO
-		}
-			
+			}
+			GUI.enabled = true;
 		}
 	}
 	
+	void TowerButtonCreate(GameObject tower, string name, int x, int y, CPUComputeTimeController controller ){
+		int cost = tower.GetComponent<TowerCost>().CPUTimeCost;
+		int time = controller.CPUTime;
+		if (time<cost) {
+				GUI.enabled = false;
+			}
+            if (GUI.Button(new Rect(x, y, 50, 50), name)) {
+						
+                controller.CPUTime -= cost;
+                selectedTower = tower;
+            }
+			GUI.enabled = true;
+	}
 	
+	void Start () {
+		yPositionModifier = yPositionInPercent/100.0f;
+		xPositionModifier = xPositionInPercent/100.0f;
+	}
  
     void Update() {
         if (Input.GetMouseButtonDown(0) && !isHoverGUI) {
