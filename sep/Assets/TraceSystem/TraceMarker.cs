6efdf604186423c46e7ec99fade922f3e07bfe39
David Asmuth <piranha771@gmail.com>
2014-01-05 14:34:23 +0100

    Traces now have visible online and offline mode.

---
+++ sep/Assets/TraceSystem/TraceMarker.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum TraceChunkType {
+    Straight,
+    Corner,
+}
+
+public class TraceMarker : MonoBehaviour {
+
+    [SerializeField]
+    private TraceChunkType type;
+
+    public TraceChunkType Type { get { return type; } set { type = value; } }
+
+
+	
+}
